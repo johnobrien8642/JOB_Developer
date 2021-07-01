f6754505fade6a5b186038ae0494192bb0d0d7fe
John O’Brien <johnobrien8642@gmail.com>
2021-07-01 15:40:16 -0700

    new ckeditor5 custom build

--- client/ckeditor5/src/ckeditor.js
+++ client/ckeditor5/src/ckeditor.js
@@ -4,13 +4,17 @@
  */
 import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
-import Markdown from '@ckeditor/ckeditor5-markdown-gfm';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
 import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import Mention from '@ckeditor/ckeditor5-mention/src/mention.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
 
 class Editor extends BalloonEditor {}
@@ -18,13 +22,17 @@ class Editor extends BalloonEditor {}
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	BlockQuote,
-	Markdown,
 	Bold,
+	CodeBlock,
 	Essentials,
+	Indent,
 	Italic,
 	Link,
-	Mention,
+	List,
+	Markdown,
+	MediaEmbed,
 	Paragraph,
+	PasteFromOffice,
 	Underline
 ];
 
