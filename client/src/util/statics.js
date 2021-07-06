497edb066c20814fd38d04b9f1af7d3e43e37764
John O’Brien <johnobrien8642@gmail.com>
2021-07-06 09:44:38 -0700

    more detailed screenshot from Rumblr

--- client/src/util/statics.js
+++ client/src/util/statics.js
@@ -1,6 +1,6 @@
 const statics = {
   mainProPic: 'http://d1k9pgunak0305.cloudfront.net/portfolio-pic-1.jpeg',
-  rumblrScreenshotThumbnail: 'http://d1k9pgunak0305.cloudfront.net/rumblr-screenshot.png',
+  rumblrScreenshotThumbnail: 'http://d1k9pgunak0305.cloudfront.net/Screen+Shot+2021-07-04+at+2.41.15+PM.png',
   squiggly: "https://img.icons8.com/material-rounded/24/000000/wavy-line.png"
 }
 
