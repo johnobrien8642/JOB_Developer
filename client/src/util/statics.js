cdd9f2a7814917bc1447b56b512e599f4c847f21
John O’Brien <johnobrien8642@gmail.com>
2021-07-07 17:41:23 -0700

    compressed rumblr thumbnail

--- client/src/util/statics.js
+++ client/src/util/statics.js
@@ -1,6 +1,6 @@
 const statics = {
   mainProPic: 'https://d1k9pgunak0305.cloudfront.net/portfolio-pic-1.jpeg',
-  rumblrScreenshotThumbnail: 'https://d1k9pgunak0305.cloudfront.net/rumblr-screenshot.png',
+  rumblrScreenshotThumbnail: 'https://d1k9pgunak0305.cloudfront.net/compressedRumblrThumbnail.png',
   squiggly: "https://img.icons8.com/material-rounded/24/000000/wavy-line.png"
 }
 
