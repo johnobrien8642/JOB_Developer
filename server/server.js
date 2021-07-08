a723b34d1bb0d862dcfc78f7491b8435b4f5cc54
John O’Brien <johnobrien8642@gmail.com>
2021-07-07 17:08:40 -0700

    back to working commit

--- server/server.js
+++ server/server.js
@@ -29,12 +29,11 @@ mongoose
 
 //repush
 
-// if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-// }
+app.use(express.static('client/build'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 
 
 cronTagFollowerHeat.start()
