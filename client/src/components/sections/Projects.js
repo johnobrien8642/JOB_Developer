5ae74dfbd93f442ef371d02d487152deb58c179b
John O’Brien <johnobrien8642@gmail.com>
2021-07-21 14:18:35 -0700

    new project cell, styling

--- client/src/components/sections/Projects.js
+++ client/src/components/sections/Projects.js
@@ -25,7 +25,7 @@ const Projects = () => {
             href='https://rumblr.app'
           >
             <img
-              className='rumblrThumbnail'
+              className='thumbnail'
               alt='rumblr project thumbnail screenshot'
               src={statics.rumblrScreenshotThumbnail}
             />
@@ -33,8 +33,33 @@ const Projects = () => {
           <p>
             My first clone and first full stack project! In this project
             I attempted to clone as many features of Tumblr as I could.
-            I also attempted to make my clone of Tumblr look as close as 
-            possible to the actual website.
+            I also attempted to make my clone of Tumblr look and function
+            as close as possible to the actual website.
+          </p>
+          <a
+            className='githubRepoLink'
+            href='https://github.com/johnobrien8642/RUMBLR_PROD'
+          >
+            Github repository
+          </a>
+        </div>
+        <div
+          className='projectCell'
+        >
+          <h2>Eight Queens Puzzle Solutions</h2>
+          <a
+            href='https://eight-queens-app-bk3o4.ondigitalocean.app'
+          >
+            <img
+              className='thumbnail'
+              alt='rumblr project thumbnail screenshot'
+              src={statics.eightQueensThumbnail}
+            />
+          </a>
+          <p>
+            A simple app that returns random solutions to
+            the Eight Queens Puzzle. I used OOP, Rails
+            with React, Bootstrap and Sass.
           </p>
           <a
             className='githubRepoLink'
--- client/src/util/statics.js
+++ client/src/util/statics.js
@@ -1,6 +1,7 @@
 const statics = {
   mainProPic: 'https://d1k9pgunak0305.cloudfront.net/portfolio-pic-1.jpeg',
   rumblrScreenshotThumbnail: 'https://d1k9pgunak0305.cloudfront.net/compressedRumblrThumbnail.png',
+  eightQueensThumbnail: 'https://d1k9pgunak0305.cloudfront.net/eightQueensThumbnail4.png',
   squiggly: "https://img.icons8.com/material-rounded/24/000000/wavy-line.png"
 }
 
